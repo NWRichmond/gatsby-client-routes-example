2c5eb46ce04591b577d229bce97757adc1e05bd7
Nick Richmond <nwrichmond@turnt.tech>
2019-10-14 13:13:17 -0400

    Add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Client Routes Example`,
+    description: `Minimal example to demonstrate client-only routes in Gatsby.`,
+    author: `@nwrichmond`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
