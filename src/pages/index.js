b734b06095ff376c9ab6f2a28264e4e170c3408b
Nick Richmond <nwrichmond@turnt.tech>
2019-10-14 13:17:11 -0400

    Update landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,25 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Home</h1>
+    <p>
+      Check out the private routes (My Account, Settings, and Billing - shown in{' '}
+      <span style={{ color: `green` }}>green</span>) to generate client-side
+      pages.
+    </p>
+    <h2>Why Client-Side?</h2>
+    <p>
+      If you have private routes with a authentication system, you want that
+      page content to be generated on-the-fly. Since we don't have a server to
+      generate content on-the-fly in a Gatsby app, the way to do this is to
+      generate the content client-side.
+    </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
